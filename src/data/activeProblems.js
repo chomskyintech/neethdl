b7b18df277391072c257a083e5fcf59978165ceb
chomskyintech <139714211+chomskyintech@users.noreply.github.com>
2026-09-15 17:23:17 +0100

    Include expanded executable problem catalog

--- src/data/activeProblems.js
+++ src/data/activeProblems.js
@@ -1,9 +1,11 @@
 import baseProblems from './problems.json'
 import acceleratorProblems from './acceleratorProblems.json'
+import expansionProblems from './expansionProblems.json'
 
 const activeProblems = [
   ...baseProblems.filter(problem => problem.evaluation?.type !== 'answer'),
   ...acceleratorProblems,
+  ...expansionProblems,
 ]
 
 export default activeProblems
