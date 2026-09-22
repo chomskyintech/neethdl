1045a7b5a73bd5331da17e2cdf5ed44f37f6a10b
chomskyintech <139714211+chomskyintech@users.noreply.github.com>
2026-09-22 23:51:02 +0100

    Load Projects styles with Projects page

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,7 @@
 import React,{useMemo,useState} from 'react'
 import{ArrowRight,ChevronRight}from'lucide-react'
 import {guidedProjects} from './data/runtimeCatalog'
+import './home-projects.css'
 
 const projectCategories=[
  {id:'all',label:'All'},
